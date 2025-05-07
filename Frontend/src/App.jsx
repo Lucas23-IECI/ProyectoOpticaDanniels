08a64dc9e9ce575c2963a709b3528790afe4bf88
Lucas23-IECI <lucas.mendez2201@alumnos.ubiobio.cl>
2025-05-07 13:23:44 -0400

    MOD: BUILD con cambios en App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          EDITA ESTO <code>src/App.jsx</code> and save to test HMR
+          ESTO ES UN CAMBIOOO <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        CAMBIO
       </p>
     </>
   )
